ffbf39e50c5b6f7835382121a856886d3e8cd4c4
Danuzia <danuziasilva30@gmail.com>
2023-05-06 14:32:30 -0300

    Estrutural final

---
+++ pop-up/script.js
@@ -0,0 +1,9 @@
+function show() {
+  let abrir = document.getElementById('modal-box');
+  abrir.style.display = 'block';
+}
+
+function hide() {
+  let fechar = document.getElementById('modal-box');
+  fechar.style.display = 'none';
+}
--- script.js
+++ script.js
@@ -4,60 +4,81 @@ let texto = document.querySelector('.decodificar');
 let final = document.querySelector('.resposta');
 let oldDiv = document.getElementsByClassName('vertical');
 let newDiv = document.getElementsByClassName('pagina2');
+const closeModalButton = document.querySelector('.close-btn');
+const modalBox = document.querySelector('#modal-box');
+const overlay = document.querySelector('#overlay');
+
+function popup() {
+  modalBox.style.display = 'flex';
+  overlay.style.opacity = 1;
+  closeModalButton.addEventListener('click', hide);
+  function hide() {
+    modalBox.style.display = 'none';
+    overlay.style.opacity = 0;
+  }
+}
 
 let encriptografar = document.querySelector('.go');
 encriptografar.onclick = function first() {
   teste = document.querySelector('.codificar');
-  let resultado = teste.value;
-  resultado = resultado.replace(/e/g, 'enter');
-  resultado = resultado.replace(/i/g, 'imes');
-  resultado = resultado.replace(/a/g, 'ai');
-  resultado = resultado.replace(/o/g, 'ober');
-  resultado = resultado.replace(/u/g, 'ufat');
-  final.value = resultado;
-  teste.value = '';
-  texto.value = '';
-  teste.focus();
-  oldDiv = document.getElementsByClassName('vertical');
-  for (var i = 0; i < oldDiv.length; i++) {
-    oldDiv[i].style.display = 'none';
-  }
-  newDiv = document.getElementsByClassName('pagina2');
-  for (var i = 0; i < newDiv.length; i++) {
-    newDiv[i].style.display = 'inline-block';
+  const regex = /[A-ZÀ-ÖØ-Þ-záàãâéèêíìîóòõôúùûÁÀÃÂÉÈÊÍÌÎÓÒÕÔÚÙÛ]/;
+  if (regex.test(teste.value)) {
+    console.log('Regex test passed!');
+    popup();
+  } else {
+    let resultado = teste.value;
+    resultado = resultado.replace(/e/g, 'enter');
+    resultado = resultado.replace(/i/g, 'imes');
+    resultado = resultado.replace(/a/g, 'ai');
+    resultado = resultado.replace(/o/g, 'ober');
+    resultado = resultado.replace(/u/g, 'ufat');
+    final.value = resultado;
+    teste.value = '';
+    texto.value = '';
+    teste.focus();
+    oldDiv = document.getElementsByClassName('vertical');
+    for (var i = 0; i < oldDiv.length; i++) {
+      oldDiv[i].style.display = 'none';
+    }
+    newDiv = document.getElementsByClassName('pagina2');
+    for (var i = 0; i < newDiv.length; i++) {
+      newDiv[i].style.display = 'inline-block';
+    }
   }
 };
 
 let descriptografar = document.querySelector('.back');
 descriptografar.onclick = function () {
   texto = document.querySelector('.decodificar');
-  let texto2 = texto.value;
-  texto2 = texto2.replace(/enter/g, 'e');
-  texto2 = texto2.replace(/imes/g, 'i');
-  texto2 = texto2.replace(/ai/g, 'a');
-  texto2 = texto2.replace(/ober/g, 'o');
-  texto2 = texto2.replace(/ufat/g, 'u');
-  final.value = texto2;
-  oldDiv = document.getElementsByClassName('vertical');
-  for (var i = 0; i < oldDiv.length; i++) {
-    oldDiv[i].style.display = 'none';
+  const regex = /[A-ZÀ-ÖØ-Þ-záàãâéèêíìîóòõôúùûÁÀÃÂÉÈÊÍÌÎÓÒÕÔÚÙÛ]/;
+  if (regex.test(texto.value)) {
+    popup();
+  } else {
+    let texto2 = texto.value;
+    texto2 = texto2.replace(/enter/g, 'e');
+    texto2 = texto2.replace(/imes/g, 'i');
+    texto2 = texto2.replace(/ai/g, 'a');
+    texto2 = texto2.replace(/ober/g, 'o');
+    texto2 = texto2.replace(/ufat/g, 'u');
+    final.value = texto2;
+    oldDiv = document.getElementsByClassName('vertical');
+    for (var i = 0; i < oldDiv.length; i++) {
+      oldDiv[i].style.display = 'none';
+    }
+    newDiv = document.getElementsByClassName('pagina2');
+    for (var i = 0; i < newDiv.length; i++) {
+      newDiv[i].style.display = 'inline-block';
+    }
+    texto.value = '';
+    teste.value = '';
+    teste.focus();
   }
-  newDiv = document.getElementsByClassName('pagina2');
-  for (var i = 0; i < newDiv.length; i++) {
-    newDiv[i].style.display = 'inline-block';
-  }
-  texto.value = '';
-  teste.value = '';
-  teste.focus();
 };
 
-let copiar = document.querySelector('.button-copy');
+let copiar = document.querySelector('.image-button');
 copiar.onclick = function () {
   let copiartexto = document.querySelector('.resposta');
   copiartexto.select();
   navigator.clipboard.writeText(copiartexto.value);
+  oldDiv = document.getElementsByClassName('vertical');
 };
-
-function myFunction() {
-  document.getElementById('myDIV').style.display = 'none';
-}
