b88ad9d344117b3dfad14fdd66d39fa7f746f771
Danuzia <75862731+Z-iE@users.noreply.github.com>
2023-05-07 10:34:46 -0300

    Delete pop-up directory
    Deletando arquivos não essenciais.

--- pop-up/script.js
+++
@@ -1,9 +0,0 @@
-function show() {
-  let abrir = document.getElementById('modal-box');
-  abrir.style.display = 'block';
-}
-
-function hide() {
-  let fechar = document.getElementById('modal-box');
-  fechar.style.display = 'none';
-}
